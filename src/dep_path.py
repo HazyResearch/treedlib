13d9d436a6530728cdff91d4c3d504c1b21adbec
Alex Ratner <ajratner@gmail.com>
2015-12-30 11:53:40 -0800

    Added root pruning

--- src/dep_path.py
+++ src/dep_path.py
@@ -5,20 +5,32 @@ import os
 APP_HOME = os.environ['TREEDLIB_HOME']
 
 class DepTree:
-  def __init__(self, sentence_input, root):
+  def __init__(self, sentence_input, prune_root=True):
     """
     Initializes via a SentenceInput object, which is the output of the CoreNLP preprocessing
     pipeline.  Crucially, a SentenceInput object should contain the words of the sentence,
     and the dependency parse in CoNLL format (two arrays, dep_parents and dep_labels)
     """
     self.s = sentence_input
-    self.tree = self._make_dep_tree(root)
+    self.tree = self._make_dep_tree(prune_root=prune_root)
 
-  def _make_dep_tree(self, root):
-    node = {'id' : root, 'text' : self.s.words[root-1], 'children' : []}
+  def _make_dep_tree(self, root=0, prune_root=False):
+    node = {
+      'id' : root,
+      'text' : self.s.words[root-1] if root > 0 else None,
+      'children' : []
+    }
     for i,d in enumerate(self.s.dep_parents):
       if d == root:
         node['children'].append(self._make_dep_tree(i+1))
+
+    # If prune_root = True, remove leaf node children of the root node
+    # In general these seem to be unimportant words (root parent is parser default?)
+    # Also collapse root node if applicable after pruning
+    if root == 0 and prune_root:
+      node['children'] = filter(lambda c : len(c['children']) > 0, node['children'])
+      if len(node['children']) == 1:
+        node = node['children'][0]
     return node
 
   def render_tree(self):
