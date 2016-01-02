c9bb4788d3739852e62be08f978e711836897745
Alex Ratner <ajratner@gmail.com>
2016-01-01 18:43:16 -0800

    Added Get method

--- src/get_features.py
+++ src/get_features.py
@@ -30,22 +30,30 @@ class BaseFeature:
   def __init__(self):
     self.label = None
     self.xpath = '//node'
-    self.attrib = []
     self.subsets = None
 
   def __repr__(self):
     return "<%s, XPath='%s', attrib=%s, subsets=%s>" % (self.label, self.xpath, self.attrib, self.subsets)
 
 
-class Self(BaseFeature):
+class Unary(BaseFeature):
   """
   The feature comprising the set of nodes making up the mention
   """
-  def __init__(self, cid, attrib=['lemma','pos']):
-    self.label = 'SELF'
+  def __init__(self, cid):
+    self.label = 'UNARY'
     self.xpath = ".//node[@cid='%s']" % str(cid)
-    self.attrib = attrib
     self.subsets = 100  # Take n-grams for *all* n...
+
+
+class Get(BaseFeature):
+  """
+  Returns the map of a specific node attribute onto the resulting node set
+  """
+  def __init__(self, f, attrib):
+    self.label = '%s-%s' % (attrib.upper(), f.label)
+    self.xpath = f.xpath + '/@' + attrib
+    self.subsets = f.subsets
 
 
 class Left(BaseFeature):
@@ -53,10 +61,9 @@ class Left(BaseFeature):
   The feature comprising the set of nodes to the left of the input feature's nodes
   Inherits the input feature's attribs if not specified otherwise
   """
-  def __init__(self, f, attrib=None):
+  def __init__(self, f):
     self.label = 'LEFT-OF-%s' % f.label
     self.xpath = f.xpath + '/preceding-sibling::node'
-    self.attrib = attrib if attrib is not None else f.attrib
     self.subsets = 3
 
 
@@ -65,10 +72,9 @@ class Right(BaseFeature):
   The feature comprising the set of nodes to the right of the input feature's nodes
   Inherits the input feature's attribs if not specified otherwise
   """
-  def __init__(self, f, attrib=None):
+  def __init__(self, f):
     self.label = 'RIGHT-OF-%s' % f.label
     self.xpath = f.xpath + '/following-sibling::node'
-    self.attrib = attrib if attrib is not None else f.attrib
     self.subsets = 3
 
 
@@ -79,12 +85,11 @@ def gen_feats(f, root):
   """
   # Optionally take subsets of the node set (e.g. n-grams) 
   res = root.xpath(f.xpath)
-  node_sets = [res] if f.subsets is None else subsets(res, f.subsets)
+  res_sets = [res] if f.subsets is None else subsets(res, f.subsets)
 
   # Generate the features
-  for nodes in node_sets:
-    for a in f.attrib:
-      yield '%s-%s[%s]' % (f.label, a, '_'.join(map(lambda node : node.get(a), nodes)))
+  for res_set in res_sets:
+    yield '%s[%s]' % (f.label, '_'.join(res_set))
 
 
 def subsets(x, L):
