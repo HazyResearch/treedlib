ad4422e538975259fba0b72ccc188807b9851c92
Alexander Ratner <ajratner@gmail.com>
2016-01-28 23:07:49 -0800

    Fixes #21 (INV_ tag), also adds dep+lemma

--- treedlib/features.py
+++ treedlib/features.py
@@ -14,12 +14,14 @@ Args: root, mention1_idxs, mention2_idxs
 get_relation_features = Compile([
   # The full dependency path between
   [Indicator(btwn, a) for a in BASIC_ATTRIBS_REL],
+  Indicator(btwn, 'dep_label,lemma'),
 
   # The *first element on the* path to the root: ngram lemmas along it
-  Ngrams(Parents(btwn, 1), 'lemma', (1,3)),
+  Ngrams(Parents(btwn, 3), 'lemma', (1,3)),
 
   # The ngrams between
   [Ngrams(btwn, a, (2,3)) for a in BASIC_ATTRIBS_REL],
+  Ngrams(btwn, 'dep_label,lemma', (2,3)),
 
   # The VBs and NNs between
   [Ngrams(Filter(btwn, 'pos', p), 'lemma', (1,3)) for p in ['VB', 'NN']],
