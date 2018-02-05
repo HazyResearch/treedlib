cee49a6696c7f24c60fa889574b18678ae6140bf
Luke Hsiao <lwhsiao@stanford.edu>
2018-02-05 11:07:18 -0800

    Add setup.py for pip

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='treedlib',
+    version='0.1.0',
+    description='Library of tree features.',
+    packages=find_packages(),
+    install_requires=[
+        'lxml',
+    ],
+    url='https://github.com/HazyResearch/treedlib',
+    author='Hazy Research',
+)
