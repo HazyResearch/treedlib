15840e2ff8e51763d7d4fb737ca7a04f66cbc3cd
Braden Hancock <hancock.braden@gmail.com>
2018-05-31 11:28:06 -0700

    Update version number to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treedlib',
-    version='0.1.0',
+    version='0.1.1',
     description='Library of tree features.',
     packages=find_packages(),
     install_requires=[
