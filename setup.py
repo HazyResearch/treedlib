a766c5975d29b473c7cf8a6bc9f285d4e6df7386
Luke Hsiao <lwhsiao@stanford.edu>
2020-06-16 16:12:39 -0700

    chore(setup.py): bump version number for v0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='treedlib',
-    version='0.1.2',
+    version='0.1.3',
     description='Library of tree features.',
     packages=find_packages(),
     install_requires=[
